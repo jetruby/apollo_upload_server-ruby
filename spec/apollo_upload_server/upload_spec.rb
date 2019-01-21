a7180804df7a21f3a44e2c8f0bfec2a072f7673a
Bertrand Dubaut <bdubaut@gmail.com>
2019-01-21 17:47:01 +0100

    Redefine Upload type with Class-based DSL

--- spec/apollo_upload_server/upload_spec.rb
+++ spec/apollo_upload_server/upload_spec.rb
@@ -2,12 +2,13 @@ require 'spec_helper'
 require 'apollo_upload_server/upload'
 
 RSpec.describe ApolloUploadServer::Upload do
+  let(:ctx) { {} }
 
   describe '#coerce_input' do
-    it { expect(described_class.coerce_input('test')).to eq 'test' }
+    it { expect(described_class.coerce_input('test', ctx)).to eq 'test' }
   end
 
   describe '#coerce_result' do
-    it { expect(described_class.coerce_result('test')).to eq 'test' }
+    it { expect(described_class.coerce_result('test', ctx)).to eq 'test' }
   end
 end
