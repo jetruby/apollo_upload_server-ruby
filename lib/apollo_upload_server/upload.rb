7fef17e7c76f96984a3b901002549ae093fa1a34
Alan Marx <59627949+Alan-Marx@users.noreply.github.com>
2022-11-15 03:51:08 -0800

    Remove unnecessary overrides (#58)

--- lib/apollo_upload_server/upload.rb
+++ lib/apollo_upload_server/upload.rb
@@ -7,13 +7,9 @@ module ApolloUploadServer
     graphql_name "Upload"
 
     def self.coerce_input(value, _ctx)
-      raise GraphQL::CoercionError, "#{value.inspect} is not a valid upload" unless value.nil? || value.is_a?(::ApolloUploadServer::Wrappers::UploadedFile)
+      return super if value.nil? || value.is_a?(::ApolloUploadServer::Wrappers::UploadedFile)
 
-      value
-    end
-
-    def self.coerce_result(value, _ctx)
-      value
+      raise GraphQL::CoercionError, "#{value.inspect} is not a valid upload"
     end
   end
 end
